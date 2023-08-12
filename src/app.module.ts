d0eadc849764009842be9a778ed8f54f15212be1
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-08-12 13:21:12 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PizzasModule } from './pizzas/pizzas.module';
+import FirstMiddleware from './middlewares/first.middleware';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URL'),
+      }),
+    }),
+    PizzasModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(FirstMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
